636ebbe0d784fcd52d82a98b7a8538c50ab8afcc
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-21 15:25:15 -0700

    initial commit

---
+++ app/functions-scripts.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Scripts
+ *
+ * @package   Ecclesiastical
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2016-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/ecclesiastical
+ */
+
+/**
+ * Define namespace
+ */
+namespace Ecclesiastical;
+
+/**
+ * Enqueue Scripts and Styles
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ *
+ * @link   https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+ * @link   https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+ */
+add_action( 'wp_enqueue_scripts', function() {
+	/**
+	 * Rather than enqueue the main stylesheet, we are going to enqueue sceen.css since all of the styles will
+	 * go here. We only need parse the information for the Theme in style.css so that it can be activated.
+	 */
+	wp_enqueue_style( 'ecclesiastical-screen', get_parent_theme_file_uri( 'public/css/screen.css' ), array(), '1.0.0' );
+
+	/**
+	 * This allows users to comment by clicking on reply so that it gets nested.
+	 */
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+} );
+
+add_action( 'wp_enqueue_scripts', function() {
+	$text_color = get_header_textcolor();
+	if ( get_theme_support( 'custom-header', 'default-text-color' ) === $text_color ) {
+		return;
+	}
+	$value      = display_header_text() ? sprintf( 'color: #%s', esc_html( $text_color ) ) : 'display: none;';
+	$custom_css = "
+		.site-branding .site-title a,
+		.site-description {
+			{$value}
+		}
+	";
+	wp_add_inline_style( 'ecclesiastical-screen', $custom_css );
+} );

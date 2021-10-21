2781b335bb511c2c62b9f3f0a1b72450d56f6eb5
Benjamin Lu <benlumia007@users.noreply.github.com>
2021-10-21 14:40:59 -0700

    initial commit

---
+++ app/framework.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Boot the framework
+ *
+ * @package   Ecclesiastical
+ * @author    Benjamin Lu ( benlumia007@gmail.com )
+ * @copyright Copyright (C) 2016-2021. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/ecclesiastical
+ */
+
+/**
+ * Create a new framework instance
+ *x
+ * This will create an instance of the framework allowing you to initialize the theme.
+ */
+$ecclesiastical = new Benlumia007\Backdrop\Framework();
+
+/**
+ * Register default providers
+ */
+$ecclesiastical->provider( Benlumia007\Backdrop\FontAwesome\Provider::class );
+$ecclesiastical->provider( Benlumia007\Backdrop\GoogleFonts\Provider::class );
+$ecclesiastical->provider( Benlumia007\Backdrop\Template\Hierarchy\Provider::class );
+$ecclesiastical->provider( Benlumia007\Backdrop\Template\Manager\Provider::class );
+$ecclesiastical->provider( Benlumia007\Backdrop\Template\View\Provider::class );
+
+/**
+ * Boot the Framework
+ */
+$ecclesiastical->boot();
